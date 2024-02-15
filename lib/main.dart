03883143e093d339009d56343f40e4e00f0183d9
Yair dahan <yairdahn@gmail.com>
2024-02-15 14:55:12 +0200

    add engel and more

--- lib/main.dart
+++ lib/main.dart
@@ -3,45 +3,50 @@ import 'dart:async';
 import 'dart:math';
 
 void main() {
-  runApp(SnakeGame());
+  runApp(const SnakeGame());
 }
 
 class SnakeGame extends StatelessWidget {
+  const SnakeGame({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Future Snake',
       theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(
-          title: Text('Future Snake'),
-        ),
-        body: Snake(),
-      ),
+          appBar: AppBar(
+            title: const Text(
+              'פלאטר סנייק',
+              textAlign: TextAlign.right,
+            ),
+          ),
+          body: const Snake()),
     );
   }
 }
 
 class Snake extends StatefulWidget {
+  const Snake({super.key});
   @override
-  _SnakeState createState() => _SnakeState();
+  SnakeState createState() => SnakeState();
 }
 
-class _SnakeState extends State<Snake> {
+class SnakeState extends State<Snake> {
   final int rows = 20;
   final int columns = 20;
   final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
   Random random = Random();
 
-  List<int> snake = [45, 65];
+  // List<int> snake = [45, 65];
+  List<int> snake = [126, 106];
   int food = 0;
-  var direction = 'up';
+  var direction = 'start';
   var isPlaying = false;
 
   void startGame() {
     const duration = Duration(milliseconds: 300);
-    snake = [45, 65];
-    snake.insert(0, snake.last);
+    snake = [126, 106];
+    snake.insert(2, snake.last);
     createFood();
     isPlaying = true;
     Timer.periodic(duration, (Timer timer) {
@@ -59,30 +64,37 @@ class _SnakeState extends State<Snake> {
   void updateSnake() {
     setState(() {
       switch (direction) {
+        case 'start':
+          snake.insert(0, snake.first + columns);
+          direction = 'down';
+          break;
+
         case 'up':
-          if (snake.first < columns) {
-            isPlaying = false;
+          if (snake.first < columns || collisionTest(snake.first, snake)) {
+            falseGame();
           } else {
             snake.insert(0, snake.first - columns);
           }
           break;
         case 'down':
-          if (snake.first >= (rows - 1) * columns) {
-            isPlaying = false;
+          if (snake.first >= (rows - 1) * columns ||
+              collisionTest(snake.first, snake)) {
+            falseGame();
           } else {
             snake.insert(0, snake.first + columns);
           }
           break;
         case 'left':
-          if (snake.first % columns == 0) {
-            isPlaying = false;
+          if (snake.first % columns == 0 || collisionTest(snake.first, snake)) {
+            falseGame();
           } else {
             snake.insert(0, snake.first - 1);
           }
           break;
         case 'right':
-          if ((snake.first + 1) % columns == 0) {
-            isPlaying = false;
+          if ((snake.first + 1) % columns == 0 ||
+              collisionTest(snake.first, snake)) {
+            falseGame();
           } else {
             snake.insert(0, snake.first + 1);
           }
@@ -99,13 +111,35 @@ class _SnakeState extends State<Snake> {
     });
   }
 
+  bool collisionTest(first, snake) {
+    var newSnake = snake.sublist(1);
+    var lost = false;
+    newSnake.forEach((element) {
+      if (element == first) {
+        lost = true;
+      }
+    });
+    return lost;
+  }
+
+  void falseGame() {
+    isPlaying = false;
+    direction = 'start';
+  }
+
   void showLostDialog() {
     showDialog(
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('נפסלת', textAlign: TextAlign.right,),
-          content: Text('לא נורא, תמיד אפשר לנסות שוב', textAlign: TextAlign.right,),
+          title: const Text(
+            'נפסלת',
+            textAlign: TextAlign.right,
+          ),
+          content: const Text(
+            'לא נורא, תמיד אפשר לנסות שוב',
+            textAlign: TextAlign.right,
+          ),
           actions: <Widget>[
             Align(
               alignment: Alignment.centerLeft,
@@ -116,7 +150,7 @@ class _SnakeState extends State<Snake> {
                     Navigator.of(context).pop();
                     startGame();
                   },
-                  child: Text('רענן'),
+                  child: const Text('שחק שוב'),
                 ),
               ),
             ),
@@ -153,33 +187,101 @@ class _SnakeState extends State<Snake> {
         body: Center(
           child: AspectRatio(
             aspectRatio: columns / (rows + 4),
-            child: Container(
-              child: GridView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: columns,
-                ),
-                itemBuilder: (BuildContext context, int index) {
-                  var color;
-                  if (snake.contains(index)) {
-                    color = Colors.green;
-                  } else if (index == food) {
-                    color = Colors.red;
-                  } else {
-                    color = Colors.grey[800];
-                  }
-                  return Container(
-                    padding: EdgeInsets.all(2),
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: Container(
-                        color: color,
-                      ),
-                    ),
-                  );
-                },
-                itemCount: rows * columns,
+            child: GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: columns,
               ),
+              itemBuilder: (BuildContext context, int index) {
+                var color;
+                // double radius = 5;
+                double radiusTopLeft = 5;
+                double radiusTopRight = 5;
+                double radiusBottomLeft = 5;
+                double radiusBottomRight = 5;
+
+                double padding = 2;
+                dynamic text = const Text('');
+                if (snake.contains(index)) {
+                  // print('snake: $snake, index: $index');
+                  color = Color.fromARGB(255, 218, 177, 84);
+                  if (index == snake.first) {
+                    color = Color.fromARGB(255, 195, 158, 74);
+                    padding = 0;
+                    // text = Text('🐍');
+                    if (direction == 'up') {
+                      text = const Text('👀');
+                      radiusBottomLeft = 0;
+                      radiusBottomRight = 0;
+                    } else if (direction == 'down') {
+                      text = const Text('👀');
+                      radiusTopLeft = 0;
+                      radiusTopRight = 0;
+                    } else if (direction == 'left') {
+                      text = Transform.rotate(
+                        angle: -pi / -2, // 90 degrees
+                        alignment: Alignment.center,
+                        child: const Text('👀'),
+                      );
+
+                      radiusBottomRight = 0;
+                      radiusTopRight = 0;
+                    } else if (direction == 'right') {
+                      text = Transform.rotate(
+                        angle: -pi / 2, // -90 degrees
+                        alignment: Alignment.center,
+                        child: const Text('👀'),
+                      );
+
+                      radiusBottomLeft = 0;
+                      radiusTopLeft = 0;
+                    } else
+                      text = const Text('🕶️');
+                  } else if (index == snake.last) {
+                    color = const Color.fromARGB(255, 51, 122, 86);
+                    padding = 0;
+                  } else {
+                    radiusTopLeft = 0;
+                    radiusTopRight = 0;
+                    radiusBottomLeft = 0;
+                    radiusBottomRight = 0;
+                    padding = 0;
+                  }
+                } else if (index == food) {
+                  text = FittedBox(
+                    child: const Text('🍎', textAlign: TextAlign.center),
+                    fit: BoxFit.contain,
+                  );
+                  color = const Color.fromARGB(255, 66, 66, 66);
+                } else {
+                  color = const Color.fromARGB(255, 66, 66, 66);
+                }
+                return Container(
+                    padding: EdgeInsets.all(padding),
+                    child: ClipRRect(
+                      // borderRadius: BorderRadius.circular(radius),
+                      borderRadius: BorderRadius.only(
+                        topLeft:
+                            Radius.circular(index == 0 ? 10 : radiusTopLeft),
+                        topRight: Radius.circular(
+                            index == columns - 1 ? 10 : radiusTopRight),
+                        bottomLeft: Radius.circular(
+                            index == columns * (rows - 1)
+                                ? 10
+                                : radiusBottomLeft),
+                        bottomRight: Radius.circular(index == columns * rows - 1
+                            ? 10
+                            : radiusBottomRight),
+                      ),
+
+                      child: Container(
+                          color: color,
+                          child: Center(
+                            child: text,
+                          )),
+                    ));
+              },
+              itemCount: rows * columns,
             ),
           ),
         ),
